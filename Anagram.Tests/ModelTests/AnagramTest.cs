3e7f4f1e113d72457a6b1f676d28f4a9a1c1f2f1
Cathy Bradley and Hyewon Cho <hyewon@>
2018-09-06 10:57:54 -0700

    Finish Anagram

--- Anagram.Tests/ModelTests/AnagramTest.cs
+++ Anagram.Tests/ModelTests/AnagramTest.cs
@@ -18,5 +18,23 @@ namespace Anagram.Tests
       Assert.AreEqual("bread", checker.getWord());
       CollectionAssert.AreEqual(new List <string> {"beard"}, checker.getWordList());
     }
+    [TestMethod]
+    public void Anagram_SplittIntoChars()
+    {
+      string word = "bread";
+      List <string> wordList = new List <string>{ "beard" };
+      AnagramChecker checker = new AnagramChecker(word, wordList);
+
+      CollectionAssert.AreEqual(new char[]{'b','r','e','a','d'}, AnagramChecker.SplitWord(word));
+    }
+    [TestMethod]
+    public void Anagram_AnagramCheck()
+    {
+      string word = "bread";
+      List <string> wordList = new List <string>{ "beard" };
+      AnagramChecker checker = new AnagramChecker(word, wordList);
+
+      CollectionAssert.AreEqual(new List <string>{ "beard" }, checker.AnagramCheck());
+    }
   }
 }
--- Anagram/Models/Anagram.cs
+++ Anagram/Models/Anagram.cs
@@ -21,12 +21,62 @@ namespace Anagram.Models
 
     public List<string> getWordList()
     {
-      return new List<string>(_wordList);
+      return _wordList;
     }
 
     public List <string> AnagramCheck()
     {
-      return new List <string>();
+      char [] wordArray = AnagramChecker.SplitWord(_word);
+      Array.Sort(wordArray);
+      string sortedWord = new string(wordArray);
+      string sortedWordInWordList = "";
+      List <string> AnagramMatch = new List <string>();
+      for(int i = 0; i < _wordList.Count; i++)
+      {
+        char [] wordListArray = AnagramChecker.SplitWord(_wordList[i]);
+        Array.Sort(wordListArray);
+        sortedWordInWordList = new string(wordListArray);
+
+        if (sortedWordInWordList.Equals(sortedWord) == true)
+        {
+          AnagramMatch.Add(_wordList[i]);
+        }
+      }
+      return AnagramMatch;
+    }
+
+    public static char[] SplitWord(string word)
+    {
+      char[] wordArray = word.ToCharArray();
+      return wordArray;
+    }
+  }
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Enter a word");
+      string firstWord = Console.ReadLine();
+      Console.WriteLine("Enter multiple words, seperated by spaces, to see if any of them are anagrams to your first word.");
+      string words = Console.ReadLine();
+      //"E" -> string
+      //'E' -> char
+      string[] wordArray = words.Split(' ');
+      
+      List <string> wordList = new List <string>();
+      foreach(string item in wordArray)
+      {
+        wordList.Add(item);
+      }
+
+      AnagramChecker mycheck = new AnagramChecker(firstWord, wordList);
+
+      List <string> result = mycheck.AnagramCheck();
+      Console.WriteLine("These words are anagrams of your first word ");
+      foreach (string word in result)
+      {
+        Console.WriteLine(word);
+      }
     }
   }
 }
